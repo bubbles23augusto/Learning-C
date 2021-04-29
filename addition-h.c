3340648cb04b6d01e7d408e34a6056971834af4d
bubbles23augsto <caugustomoro@gmail.com>
2021-04-29 10:13:03 -0300

    "simple int adding"

---
+++ addition-h.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+	int x;
+	int y;
+	printf("Digite o primeiro valor inteiro da soma:");
+	scanf("%i", &x);
+	printf("Digite o segundo valor inteiro da soma:");
+	scanf("%i", &y);
+	printf("%i\n" , x + y);
+	return(0);
+}
+
