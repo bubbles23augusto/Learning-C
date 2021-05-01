54bc06c68a8544a79959b9e34cf4a941a3f0feb6
bubbles23augsto <caugustomoro@gmail.com>
2021-05-01 10:27:01 -0300

    "correcoes no programa de escrever o alfabeto"

--- writeZtoA.c
+++ writeZtoA.c
@@ -3,12 +3,12 @@
 int main(void)
 {
 	//o contador que vai parar o while serve como inicio na posição correta, utilizando 97, tabela ascii = a
-	int contador = 97;
-	while(contador <123)
+	int contador = 122;
+	while(contador > 96)
 	{
 		//aqui eu digo pro write: amigo, não quero que você escreva o inteiro, mas o endereço, o apontador de "97", que no caso é "a" no ascii
 		write(1, &contador , 1);
-		contador++;
+		contador--;
 	}
 	write(1, "\n" , 1);
 	return (0);
