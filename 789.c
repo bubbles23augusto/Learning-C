12c1cd1495bd0c795b761dfa0f13f6b096edd140
bubbles23augsto <caugustomoro@gmail.com>
2021-05-10 08:34:58 -0300

    "write number possibilities"

---
+++ 789.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+void numberwrite(int unit, int dezena, int centena) {
+		
+		char unichar = unit + '0' ;
+		char chardezena = dezena + '0' ;
+		char charcentena = centena +'0';
+		
+		write(1, &charcentena, 1) ;
+		write(1, &chardezena, 1) ;
+		write(1, &unichar, 1) ;	
+		write(1, ", ", 2) ;
+}
+
+int main (void) {
+	
+	int unit = 0 ;
+	int dezena = 0 ;
+	int centena = 0 ;
+	
+	/*here we start the loop of the hundreds*/
+	while (centena <= 9){
+		/*here we start the loop of the tens*/
+		while (dezena <= 9) {
+			/*here we exclude the equal values*/
+			if (dezena <= centena) {
+				dezena++ ;
+			}
+			else {
+				/*starting the loop of the units*/
+				while (unit <= 9) {
+					/*excluding equal values*/
+					if (unit <= centena || unit <= dezena) {
+						unit++;
+					}
+					else {
+						numberwrite(unit, dezena, centena) ;
+						unit ++ ;
+					}
+				}
+			
+				dezena++;
+				unit = 0;
+			}
+		}
+		
+	centena++ ;
+	dezena = 0;
+	}
+	
+	return(0) ;
+}
