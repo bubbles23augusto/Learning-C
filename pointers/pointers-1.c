086db829fcd1f3a0ab2b2190debe57800b9acc0d
bubbles <caugustomoro@gmail.com>
2021-05-31 13:45:14 -0300

    "some code involving pointers"

---
+++ pointers/pointers-1.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void intpointer(int* number)
+{
+	/*changed the value of the pointer to 42, so n in main will change too*/
+	*number = 42;
+}
+
+int main (void)
+{
+	/* n is the variable, *p is the pointer.*/
+	int n=0;
+	int *p;
+	printf("Before: %i\n", n);
+	/* p is pointing to the n adddres, so any changes in p, reflect on n */
+	p=&n;
+	
+	intpointer(p);
+
+	printf("After: %i\n", n);
+	
+	return(0);
+}
